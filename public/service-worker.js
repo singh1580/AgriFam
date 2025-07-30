50f69aa0d3a05ea82e94c33427ead9866e83f8c8
kingsingh1433 <kingsingh1433@gmail.com>
2025-07-30 21:16:27 +0000

    Changes before Firebase Studio auto-run

---
+++ public/service-worker.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'agrifam-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+  // Add other assets you want to cache (CSS, JS, images, etc.)
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response; // Found in cache
+        }
+        return fetch(event.request); // Not in cache, fetch from network
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Delete old caches
+          }
+        })
+      );
+    })
+  );
+});
